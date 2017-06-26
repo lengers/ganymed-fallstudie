0a100f3f8e81390a1532fb5cba6b7fbacdcc1414
lengers <leo@engers.de>
2017-06-26 19:06:02 +0200

    Fixed formatting

--- components/routes/api.js
+++ components/routes/api.js
@@ -24,22 +24,23 @@ api.use(bodyParser.urlencoded({ // to support URL-encoded bodies
     extended: true
 }));
 
-function check_jwt(token) {
-    jwt.verify(token, secret, function(err, decoded) {
-        if (err) {
-            console.log(err);
-            return "invalid";
-        } else {
-            console.log(decoded);
-            return decoded;
-        }
-    });
-}
+// const check_jwt = (next, token) => {
+//     jwt.verify(token, secret, (err, decoded) => {
+//         if (err) {
+//             console.log(err);
+//             return "invalid";
+//         } else {
+//             console.log(decoded);
+//             return decoded;
+//         }
+//     });
+// }
 
-// Mocking scans requires these variables
-var scan_uuid = "";
-var scan_start = "";
-var scan_percent = 0;
+
+// Mocking scans requires these constiables
+let scan_uuid = "";
+let scan_start = "";
+let scan_percent = 0;
 
 api
     .get('/help', (req, res) => {
@@ -48,14 +49,14 @@ api
         })
     })
     .post('/auth', (req, res) => {
-        connection.query('SELECT * FROM user WHERE username = ?', [req.body.name], function(error, results, fields) {
+        connection.query('SELECT * FROM user WHERE username = ?', [req.body.name], (error, results, fields) => {
             if (error) throw error;
-            var hash = results[0].hash;
-            var password_correct = bcrypt.compareSync(req.body.password, results[0].hash);
+            const hash = results[0].hash;
+            const password_correct = bcrypt.compareSync(req.body.password, results[0].hash);
             if (password_correct) {
                 // TODO: Implement groups like:
-                //   var user_token = jwt.sign({user: req.params.name, group: TODO}, secret);
-                var user_token = jwt.sign({
+                //   const user_token = jwt.sign({user: req.params.name, group: TODO}, secret);
+                const user_token = jwt.sign({
                     exp: Math.floor(Date.now() / 1000) + (60 * 60 * 13),
                     name: req.body.name,
                     group: results[0].group
@@ -75,7 +76,7 @@ api
         });
     })
     .get('/auth/check', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 console.log("Token invalid.");
                 res.status(200).json({
@@ -91,7 +92,7 @@ api
         });
     })
     .get('/devices', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 res.status(403).json({
                     status: "error",
@@ -99,7 +100,7 @@ api
                 })
             } else {
                 try {
-                    connection.query('SELECT * FROM device;', function(error, results, fields) {
+                    connection.query('SELECT * FROM device;', (error, results, fields) => {
                         if (error) throw error;
                         res.status(200).json({
                             status: "ok",
@@ -115,16 +116,16 @@ api
         });
     })
     .post('/devices/create', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 res.status(403).json({
                     status: "error",
                     data: "You are not authorized to access this endpoint"
                 })
             } else {
-                var create_query = "INSERT INTO device (uuid, ip, mac, ports, risk_level, services) VALUES (?, ?, ?, ?, ? , ?);"
+                const create_query = "INSERT INTO device (uuid, ip, mac, ports, risk_level, services) VALUES (?, ?, ?, ?, ? , ?);"
                 try {
-                    connection.query(create_query, [req.body.uuid, req.body.ip, req.body.mac, req.body.manufacturer, req.body.ports, req.body.risk_level, req.body.services, req.body.name, req.body.modell], function(error, results, fields) {
+                    connection.query(create_query, [req.body.uuid, req.body.ip, req.body.mac, req.body.manufacturer, req.body.ports, req.body.risk_level, req.body.services, req.body.name, req.body.modell], (error, results, fields) => {
                         if (error) {
                             throw error;
                             res.status(500).send("An error occured: " + error);
@@ -139,16 +140,16 @@ api
         });
     })
     .post('/devices/remove', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 res.status(403).json({
                     status: "error",
                     data: "You are not authorized to access this endpoint"
                 })
             } else {
-                var create_query = "DELETE FROM device WHERE uuid = ?;"
+                const create_query = "DELETE FROM device WHERE uuid = ?;"
                 try {
-                    connection.query(create_query, [req.body.uuid], function(error, results, fields) {
+                    connection.query(create_query, [req.body.uuid], (error, results, fields) => {
                         if (error) {
                             throw error;
                             res.status(500).send("An error occured: " + error);
@@ -164,19 +165,19 @@ api
     })
     // Expects {"token": "$2a$10$9B3aQ.iG8ekCH34yiIt9k.8D.EdMDIyMYenQRYr.sMsyzyA0B38p.","new": {"name": "testuser","password": "testpassword", "group": "group", "mail": "mail"}}
     .post('/users/create', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
-            if (err) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
+            if ((err) || (decoded.group !== "admin")) {
                 res.status(403).json({
                     status: "error",
                     data: "You are not authorized to access this endpoint"
                 })
             } else {
                 // Create password hash
-                var hash = bcrypt.hashSync(req.body.new.password, 10);
+                const hash = bcrypt.hashSync(req.body.new.password, 10);
                 try {
                     // Create new user
-                    var create_query = "INSERT INTO user (username, `hash`, `group`, mail) VALUES (?, ?, ?, ?);"
-                    connection.query(create_query, [req.body.new.name, hash, req.body.new.group, req.body.new.mail], function(error, results, fields) {
+                    const create_query = "INSERT INTO user (username, `hash`, `group`, mail) VALUES (?, ?, ?, ?);"
+                    connection.query(create_query, [req.body.new.name, hash, req.body.new.group, req.body.new.mail], (error, results, fields) => {
                         if (error) {
                             throw error;
                             res.status(500).json({
@@ -200,7 +201,7 @@ api
         });
     })
     .get('/scan/start', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 res.status(403).json({
                     status: "error",
@@ -226,7 +227,7 @@ api
         });
     })
     .get('/scan/status/:uuid', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 res.status(403).json({
                     status: "error",
@@ -240,7 +241,7 @@ api
                         data: "No scan with matching UUID found."
                     })
                 } else {
-                    var timestamp = Math.floor(Date.now() / 1000);
+                    const timestamp = Math.floor(Date.now() / 1000);
                     scan_percent = Math.floor((timestamp - scan_start) / 120 * 100);
                     if (scan_percent > 100) {
                         scan_percent = 100
@@ -259,7 +260,7 @@ api
         });
     })
     .get('/scan/results/:uuid', (req, res) => {
-        jwt.verify(req.headers.token, secret, function(err, decoded) {
+        jwt.verify(req.headers.token, secret, (err, decoded) => {
             if (err) {
                 res.status(403).json({
                     status: "error",
@@ -282,8 +283,8 @@ api
                         // Write scan info to DB
                         try {
                             // Create new user
-                            var create_query = "INSERT INTO scan (scan_no, start_time, started_by_user, duration, risk_level) VALUES (?, ?, ?, ?, ?);"
-                            connection.query(create_query, [scan_uuid, scan_start, local_token.name, 120, 2], function(error, results, fields) {
+                            const create_query = "INSERT INTO scan (scan_no, start_time, started_by_user, duration, risk_level) VALUES (?, ?, ?, ?, ?);"
+                            connection.query(create_query, [scan_uuid, scan_start, decoded.name, 120, 2], (error, results, fields) => {
                                 if (error) {throw error}
                             });
                         } catch (e) {
@@ -293,7 +294,7 @@ api
                             })
                         }
                         // Read scan results from file
-                        var scanpath = path.join(__dirname, '..', '..', 'public', 'assets', 'mock', 'scan.json');
+                        const scanpath = path.join(__dirname, '..', '..', 'public', 'assets', 'mock', 'scan.json');
                         fs.readFile(scanpath, (err, scanresults) => {
                             if (err) throw err;
                             // Send answer
@@ -308,16 +309,16 @@ api
         });
     })
     .get('/debug/reset', (req, res) => {
-        var sqlpath = path.join(__dirname, '..', '..', 'ganymed.sql');
+        const sqlpath = path.join(__dirname, '..', '..', 'ganymed.sql');
         fs.readFile(sqlpath, (err, sqlquery) => {
             if (err) throw err;
             // Replace all emtpy elements in array
-            var query = sqlquery.toString().replace(/(?:\r\n|\r|\n)/g, ' ');
-            var query_array = query.split(";").filter(function(e){return /\S/.test(e)});
+            const query = sqlquery.toString().replace(/(?:\r\n|\r|\n)/g, ' ');
+            const query_array = query.split(";").filter(function(e){return /\S/.test(e)});
             try {
-                for (var i = 0; i < query_array.length; i++) {
+                for (const i = 0; i < query_array.length; i++) {
                     console.log("EXECUTING QUERY: " + query_array[i]);
-                    connection.query(query_array[i] + ";", function(error, results, fields) {
+                    connection.query(query_array[i] + ";", (error, results, fields) => {
                             if (error) throw error;
                         });
                 };
